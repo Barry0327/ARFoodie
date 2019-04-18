0f57da85f53cb473a665fed37c3e56606b83e7df
Barry <fm334142@gmail.com>
2019-04-18 12:35:02 +0800

    初步完成留言功能

---
+++ ARFoodie/Controller/RegisterViewController + handlers.swift
@@ -0,0 +1,9 @@
+//
+//  RegisterViewController + handlers.swift
+//  ARFoodie
+//
+//  Created by Chen Yi-Wei on 2019/4/18.
+//  Copyright © 2019 Chen Yi-Wei. All rights reserved.
+//
+
+import Foundation
---
+++ ARFoodie/Model/Comment.swift
@@ -0,0 +1,50 @@
+//
+//  Comment.swift
+//  ARFoodie
+//
+//  Created by Chen Yi-Wei on 2019/4/18.
+//  Copyright © 2019 Chen Yi-Wei. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+struct Comment {
+
+    let senderName: String
+
+    let senderUid: String
+
+    let content: String
+
+    init(name: String, uid: String, content: String) {
+
+        self.senderName = name
+        self.senderUid = uid
+        self.content = content
+    }
+
+    init?(snapshot: DataSnapshot) {
+
+        guard
+            let value = snapshot.value as? [String: Any],
+            let senderName = value["senderName"] as? String,
+            let senderUid = value["senderUid"] as? String,
+            let content = value["content"] as? String
+            else { return nil }
+
+        self.senderName = senderName
+        self.senderUid = senderUid
+        self.content = content
+
+    }
+
+    func toAnyObject() -> Any {
+        return [
+            "senderName": self.senderName,
+            "senderUid": self.senderUid,
+            "content": self.content
+        ]
+    }
+
+}
