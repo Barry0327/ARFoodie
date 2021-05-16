7e4a227fabcc7729a9312308b1315825d13af783
Barry Chen <fm334142@gmail.com>
2021-05-16 23:27:56 +0800

    chore: ignore some data

--- Pods/Target Support Files/IHProgressHUD/IHProgressHUD-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_IHProgressHUD : NSObject
-@end
-@implementation PodsDummy_IHProgressHUD
-@end
--- Pods/Target Support Files/YTLiveStreaming/YTLiveStreaming-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_YTLiveStreaming : NSObject
-@end
-@implementation PodsDummy_YTLiveStreaming
-@end
