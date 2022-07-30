3f4f58020a0f8c5125d5ff1ee403bfcf2521c4b8
Barry Chen <fm334142@gmail.com>
2022-07-30 23:48:59 +0800

    Request data from url once call load method

--- ARFoodie/ARFoodieTests/RemoteRestaurantLoaderTests.swift
+++ ARFoodie/ARFoodieTests/RemoteRestaurantLoaderTests.swift
@@ -15,31 +15,48 @@ protocol HTTPClient {
 }
 
 class RemoteRestaurantLoader {
+    let url: URL
     let client: HTTPClient
 
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
+    }
+
+    func load() {
+        client.get(url: url, completion: { _ in })
     }
 }
 
 class RemoteRestaurantLoaderTests: XCTestCase {
 
     func test_init_dosetNotRequestDataFromURL() {
+        let anyURL = URL(string: "https://any.com")!
         let client = HTTPClientSpy()
-        let _ = RemoteRestaurantLoader(client: client)
+        let _ = RemoteRestaurantLoader(url: anyURL, client: client)
 
         XCTAssertEqual(client.requestCallCount, 0)
     }
 
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
+        let client = HTTPClientSpy()
+        let sut = RemoteRestaurantLoader(url: url, client: client)
+
+        sut.load()
+
+        XCTAssertEqual(client.receivedURLs, [url])
+    }
+
     class HTTPClientSpy: HTTPClient {
-        private var receivedURLs: [URL] = []
+        private(set) var receivedURLs: [URL] = []
 
         var requestCallCount: Int {
             receivedURLs.count
         }
 
         func get(url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-
+            receivedURLs.append(url)
         }
     }
 }
