e2cfada0e72c77410bb171a36617bfe561173980
Barry Chen <fm334142@gmail.com>
2022-07-30 22:48:46 +0800

    Add `ARFoodie` project

---
+++ ARFoodie/ARFoodie/ARFoodie.h
@@ -0,0 +1,18 @@
+//
+//  ARFoodie.h
+//  ARFoodie
+//
+//  Created by Chen Yi-Wei on 2022/7/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ARFoodie.
+FOUNDATION_EXPORT double ARFoodieVersionNumber;
+
+//! Project version string for ARFoodie.
+FOUNDATION_EXPORT const unsigned char ARFoodieVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARFoodie/PublicHeader.h>
+
+
