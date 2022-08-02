94fc4925de939c469cd9bdced3fa76a493933563
Barry Chen <fm334142@gmail.com>
2022-08-02 23:50:23 +0800

    Move mapping logic to new type `RestaurantMapper`

--- ARFoodie/RemoteRestaurantLoader.swift
+++ ARFoodie/RemoteRestaurantLoader.swift
@@ -27,50 +27,17 @@ public class RemoteRestaurantLoader {
 
     public func load(completion: @escaping (Result) -> Void) {
         client.get(url: url) { [weak self] result in
-            guard let self = self else { return }
             switch result {
             case let .success((data, response)):
-                guard self.validStatusCodes.contains(response.statusCode) else {
-                    completion(.failure(.invalidData))
-                    return
-                }
-                let decoder = JSONDecoder()
-                if let items = try? decoder.decode(Root.self, from: data).items {
+                do {
+                    let items = try RestaurantMapper.map(data, response)
                     completion(.success(items))
-                }
-                else {
+                } catch {
                     completion(.failure(.invalidData))
                 }
             case .failure:
                 completion(.failure(.connectionError))
             }
         }
-    }
-}
-
-private struct Root: Decodable {
-    let results: [RemoteRestaurant]
-
-    var items: [Restaurant] {
-        results.map { $0.restaurant }
-    }
-}
-
-private struct RemoteRestaurant: Decodable {
-    struct Location: Decodable {
-        let lat: Double
-        let lng: Double
-    }
-    struct Geometry: Decodable {
-        let location: Location
-    }
-    let place_id: String
-    let name: String
-    let geometry: Geometry
-    let rating: Double?
-    let user_ratings_total: Double?
-
-    var restaurant: Restaurant {
-        .init(id: place_id, name: name, latitude: geometry.location.lat, longitude: geometry.location.lng, rating: rating, userRatingsTotal: user_ratings_total)
     }
 }
