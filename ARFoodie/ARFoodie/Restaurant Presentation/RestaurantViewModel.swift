47036a38e79ef1d9f214137e41617eab1388d5aa
Barry0327 <47265371+Barry0327@users.noreply.github.com>
2022-08-18 11:56:01 +0800

    Add `RestaurantViewModel` and tests

---
+++ ARFoodie/ARFoodie/Restaurant Presentation/RestaurantViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RestaurantViewModel.swift
+//  ARFoodie
+//
+//  Created by Chen Yi-Wei on 2022/8/18.
+//
+
+import Foundation
+
+final class RestaurantViewModel {
+    
+}
---
+++ ARFoodie/ARFoodieTests/Restaurant Presentation/RestaurantViewModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  RestaurantViewModelTests.swift
+//  ARFoodieTests
+//
+//  Created by Chen Yi-Wei on 2022/8/18.
+//
+
+import XCTest
+
+class RestaurantViewModelTests: XCTestCase {
+
+}
+ 
