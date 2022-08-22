8bed70c9dac55e60e980eb1155fd82d7c4be1849
Barry0327 <47265371+Barry0327@users.noreply.github.com>
2022-08-22 17:45:18 +0800

    Implement `CLLocationService`

---
+++ ARFoodie/ARFoodieiOS/CLLocationService.swift
@@ -0,0 +1,61 @@
+//
+//  CLLocationService.swift
+//  ARFoodie
+//
+//  Created by Chen Yi-Wei on 2022/8/22.
+//
+
+import Foundation
+import CoreLocation
+
+public final class CLLocationService: NSObject {
+    typealias Completion = (Result<Coordinate, Error>) -> Void
+    private(set) lazy var manager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.delegate = self
+        return manager
+    }()
+
+    private var completion: Completion?
+
+    public enum Error: Swift.Error {
+        case notAuthorized
+        case invalidLocation
+    }
+
+    func getLocation(completion: @escaping Completion) {
+        let authorizationStatus = manager.authorizationStatus
+        guard authorizationStatus == .authorizedWhenInUse else {
+            return completion(.failure(Error.notAuthorized))
+        }
+
+        self.completion = completion
+        manager.requestLocation()
+    }
+}
+extension CLLocationService: CLLocationManagerDelegate {
+    public func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        default: return
+        }
+    }
+
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last,
+              location.horizontalAccuracy > 0
+        else {
+            completion?(.failure(Error.invalidLocation))
+            return completion = nil
+        }
+
+        let coordinate = Coordinate(
+            longitude: location.coordinate.longitude,
+            latitude: location.coordinate.latitude
+        )
+        completion?(.success(coordinate))
+        completion = nil
+    }
+
+}
