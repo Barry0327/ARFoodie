89eecd33e08877072fdddbfb04f6aaf1c067aa78
Barry0327 <47265371+Barry0327@users.noreply.github.com>
2022-08-22 11:28:13 +0800

    Add `RestaurantSnapshotTests`

---
+++ ARFoodie/ARFoodieiOSTests/Restaurant UI/RestaurantSnapshotTests.swift
@@ -0,0 +1,15 @@
+//
+//  RestaurantSnapshotTests.swift
+//  RestaurantSnapshotTests
+//
+//  Created by Chen Yi-Wei on 2022/8/9.
+//
+
+import XCTest
+import ARFoodieiOS
+
+class RestaurantSnapshotTests: XCTestCase {
+    func test_init() {
+        XCTFail("")
+    }
+}
