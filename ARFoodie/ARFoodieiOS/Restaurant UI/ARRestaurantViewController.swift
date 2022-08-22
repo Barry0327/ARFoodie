9162d251cabe401b445895492ebc386f65a877b1
Barry0327 <47265371+Barry0327@users.noreply.github.com>
2022-08-22 11:29:33 +0800

    Add `ARRestaurantViewController`

---
+++ ARFoodie/ARFoodieiOS/Restaurant UI/ARRestaurantViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ARRestaurantViewController.swift
+//  ARFoodieiOS
+//
+//  Created by Chen Yi-Wei on 2022/8/22.
+//
+
+import UIKit
+
+public final class ARRestaurantViewController: UIViewController {
+    
+}
