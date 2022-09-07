55dbcb4e90e10211760cf78f7f5a7b2670209cf1
Barry0327 <47265371+Barry0327@users.noreply.github.com>
2022-09-07 18:31:59 +0800

    Add `ARRestaurantUIIntegrationTests`

---
+++ ARFoodieAppTests/ARRestaurantUIIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  ARRestaurantUIIntegrationTests.swift
+//  ARFoodieAppTests
+//
+//  Created by Chen Yi-Wei on 2022/9/7.
+//  Copyright © 2022 Chen Yi-Wei. All rights reserved.
+//
+
+import XCTest
+
+class ARRestaurantUIIntegrationTests: XCTestCase {
+    
+}
